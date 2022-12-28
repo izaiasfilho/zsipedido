df23c9b197b3987737f2cc0c01a3f896d66b929d
izaias filho <izs.af@hotmail.com>
2022-12-28 20:07:49 -0300

    primeiro commit - projeto base

---
+++ src/main/java/com/izs/zsipedidos/ZsipedidosApplication.java
@@ -0,0 +1,13 @@
+package com.izs.zsipedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsipedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsipedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izs/zsipedidos/ZsipedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izs.zsipedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZsipedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
