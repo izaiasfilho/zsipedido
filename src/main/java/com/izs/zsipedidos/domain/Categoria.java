06fa8436368ae711ba1c94ac7163d66ee40eacfe
izaias filho <izs.af@hotmail.com>
2022-12-28 22:44:55 -0300

    banco de dados h2 e criacao automatica da base de dados

--- src/main/java/com/izs/zsipedidos/domain/Categoria.java
+++ src/main/java/com/izs/zsipedidos/domain/Categoria.java
@@ -2,14 +2,22 @@ package com.izs.zsipedidos.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+
 @Data
 @EqualsAndHashCode
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
