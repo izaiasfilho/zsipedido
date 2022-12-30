d0a873c15b6d1da9c0bf8560bb676643ee9a4ce4
izaias filho <izs.af@hotmail.com>
2022-12-29 21:06:08 -0300

    criando operacao de instanciacao

--- src/main/java/com/izs/zsipedidos/ZsipedidosApplication.java
+++ src/main/java/com/izs/zsipedidos/ZsipedidosApplication.java
@@ -1,13 +1,30 @@
 package com.izs.zsipedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ZsipedidosApplication {
+import com.izs.zsipedidos.domain.Categoria;
+import com.izs.zsipedidos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ZsipedidosApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ZsipedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		repo.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
