d6684da9ab3121b42dca49a2e9b47a13ed1f8a1f
izaias filho <izs.af@hotmail.com>
2023-01-10 22:14:08 -0300

    correcoes: update em categoriaservice e construtor de pagamento

--- src/main/java/com/izs/zsipedidos/services/CategoriaService.java
+++ src/main/java/com/izs/zsipedidos/services/CategoriaService.java
@@ -31,8 +31,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria categoria) {
-		find(categoria.getId());
-		return repo.save(categoria);
+		Categoria categoriaBD = find(categoria.getId());
+		updateData(categoriaBD,categoria);
+		return repo.save(categoriaBD);
 	}
 
     public void delete(Integer id) {
@@ -58,6 +59,13 @@ public class CategoriaService {
     public Categoria fromDTO(CategoriaDTO dto) {
     	return new Categoria(dto.getId(), dto.getNome());
     }
+    
+    
+    private void updateData(Categoria categoriaBD, Categoria categoria) {
+    	categoriaBD.setNome(categoria.getNome());
+
+    }
+
 }
 
 
